aae0e019b15ef785a3753383847b3da5cb2c977f
Genevieve Conty <gmireillec@Genevieves-MacBook-Pro.local>
2016-10-07 18:13:23 -0400

    prints just fine, inquirer modules, on to prompts

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var inquirer = require('inquirer');
 
 var connection = mysql.createConnection({
 	host: "localhost",
@@ -11,14 +12,43 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
+    displayStock();
+
+
 })
 
+function displayStock(){
+	connection.query('SELECT * FROM Products', function(err, res) {
+	    for (var i = 0; i < res.length; i++) {
+	        console.log(res[i].id + " | " + res[i].productName + " | " + res[i].price);
+	    }
 
-// display inventory with id, names, prices
+    	console.log("-----------------------------------");
+	});
+};
 
 // using prompt npm, promt two messages
 // ask for ID
 // ask for how many they want
+
+function VAR## (){
+    inquirer.prompt([{
+           name: "ID",
+           message: "What's the product id for your item?"
+       }, {
+           name: "quantity",
+           message: "How many would you like?"
+       }]).then(function(answers){
+
+		       	connection.query('SELECT * FROM Top5000 WHERE artist=?', [answers.artist], function(err, res) {
+				    for (var i = 0; i < res.length; i++) {
+				        console.log(res[i])
+				        // console.log(res[i].position + " | " + res[i].artist + " | " + res[i].song + " | " + res[i].release + " | " + res[i].1rating + " | " + res[i].2rating + " | " + res[i].3rating + " | " + res[i].4rating + " | " + res[i].5rating);
+				    }
+				})
+	  		})
+}
+
 
 // check for sufficent inventory
 	// fill order (stockQuantity--)
