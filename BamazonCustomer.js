6103633b359302534ff2351901e478a10ff51e6e
Genevieve Conty <gmireillec@Genevieves-MacBook-Pro.local>
2016-10-07 16:04:44 -0400

    stupid mistake fixed, connecting to database fine now

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -1,3 +1,19 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+	host: "localhost",
+	port: 3306,
+	user: 'root',
+	password: '',
+	database: 'Bamazon'
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+})
+
+
 // display inventory with id, names, prices
 
 // using prompt npm, promt two messages
@@ -8,3 +24,4 @@
 	// fill order (stockQuantity--)
 	// show total cost of purchase
 // else "Insifficient quantity"
+
