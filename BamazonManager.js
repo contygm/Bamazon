89e0c5f82f2ce9d3fab2ba1470b971f0fa065a00
Genevieve Conty <gmireillec@Genevieves-MacBook-Pro.local>
2016-10-08 05:05:46 -0400

    copied over basic set up, set up initial prompt

--- BamazonManager.js
+++ BamazonManager.js
@@ -9,3 +9,81 @@
 		// can add totall new products
 
 
+var mysql = require("mysql");
+var inquirer = require('inquirer');
+
+var connection = mysql.createConnection({
+	host: "localhost",
+	port: 3306,
+	user: 'root',
+	password: '',
+	database: 'Bamazon'
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+
+    console.log("connection locked");
+})
+
+function pickProcess(){
+	inquirer.prompt([{
+           name: "chosenProcess",
+           type: "list",
+           message: "What's the product id for your item?",
+           choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"]
+       }]).then(function(answers)){
+			switch(answers.chosenProcess){
+				case 'View Products for Sale':
+					displayStock();
+					break;
+
+				case 'View Low Inventory':
+					lowStock();
+					break;
+
+				case 'Add to Inventory':
+					addStock();
+					break;
+
+				case 'Add New Product':
+					newProduct();
+					break;
+
+				default:
+					console.log("Good job holmes, you broke it!");
+			}
+	};
+}
+
+function displayStock(){
+	connection.query('SELECT * FROM Products', function(err, res) {
+	    for (var i = 0; i < res.length; i++) {
+	        console.log(res[i].id + " | " + res[i].productName + " | " + res[i].price + " | " + res[i].quantities);
+	    }
+
+    	console.log("-----------------------------------");
+	});
+
+};
+
+function lowStock(){
+	connection.query('SELECT * FROM Products WHERE stockQuantity', function(err, res) {
+		    for (var i = 0; i < res.length; i++) {
+		        console.log(res[i].id + " | " + res[i].productName + " | " + res[i].price + " | " + res[i].quantities);
+		    }
+
+	    	console.log("-----------------------------------");
+	});
+}
+
+// adds to stockQuantity
+function addStock(){
+
+}
+
+// adds whole new item
+function newProduct{
+
+}
+
