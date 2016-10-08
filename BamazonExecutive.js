a450e9be489663fad498fcc292b8bbad3c199ea4
Genevieve Conty <gmireillec@Genevieves-MacBook-Pro.local>
2016-10-08 06:23:04 -0400

    formating table

--- BamazonExecutive.js
+++ BamazonExecutive.js
@@ -10,43 +10,15 @@ var connection = mysql.createConnection({
 	database: 'Bamazon'
 });
 
-// instantiate 
-var table1 = new Table({
-    head: ['TH 1 label', 'TH 2 label']
-  , colWidths: [100, 200]
-});
+var table = new Table({ head: ["Department ID", "Department Name", "Over Head Costs", "Product Sales", "Total Profit"] });
  
-// table is an Array, so you can `push`, `unshift`, `splice` and friends 
-table1.push(
-    ['First value', 'Second value']
-  , ['First value', 'Second value']
-);
-
-console.log(table1.toString());
-
-console.log("----------------1-------------------");
-
-var table2 = new Table();
- 
-table2.push(
-    { 'Some key': 'Some value' }
-  , { 'Another key': 'Another value' }
+table.push(
+    { 'id#': ['Name', 'overHeadCosts','Sales', 'Profit'] }
+  , { 'id#': ['Name', 'overHeadCosts','Sales', 'Profit'] }
 );
  
-console.log(table2.toString());
+console.log(table.toString());
 
-console.log("-----------------2------------------");
-
-var table3 = new Table({ head: ["", "Top Header 1", "Top Header 2"] });
- 
-table3.push(
-    { 'Left Header 1': ['Value Row 1 Col 1', 'Value Row 1 Col 2'] }
-  , { 'Left Header 2': ['Value Row 2 Col 1', 'Value Row 2 Col 2'] }
-);
- 
-console.log(table3.toString());
-
-console.log("-----------------3------------------");
 // Running this application will list a set of menu options:
 	// View Product Sales by Department
 	// Create New Department
